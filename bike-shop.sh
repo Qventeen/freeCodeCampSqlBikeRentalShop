5d2117e50d5d743cef283f1e74607ff43909f3b0
SerhiiTuhaienko <serhiituhaienko@gmail.com>
2023-08-24 09:49:18 -0400

    Save progress: 1220.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -42,7 +42,6 @@ RENT_MENU() {
         else
             #get bike availability
             BIKE_AVAILABILITY=$($PSQL "select available from bikes where bike_id = $BIKE_ID_TO_RENT and available = true")
-            echo $BIKE_AVAILABILITY
             #if not available
             if [[ -z $BIKE_AVAILABILITY ]]
             then
